da2e471680ab5d935ec1538a7e7bd8b2280525d2
Lars Magne Ingebrigtsen <larsi@quimbies.gnus.org>
2011-04-16 04:33:26 +0200

    port stuff from jukebox

---
+++ dae.el
@@ -0,0 +1,32 @@
+;;; dae.el --- Digital Audio Extraction
+;; Copyright (C) 2011 Lars Magne Ingebrigtsen
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: music
+
+;; This file is not part of GNU Emacs.
+
+;; dae.el is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; dae.el is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with dae.el; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+;; 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl)
+
+(provide 'dae)
+
+;;; dae.el ends here
